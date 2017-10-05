5881a06c6e41e43ee1775682628c38471bd7d2aa
Ticha Sethapakdi <ticha.seth@gmail.com>
2017-10-05 04:49:58 -0400

    i am so tired

--- Project1/Project1.ino
+++ Project1/Project1.ino
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <avr/pgmspace.h>
 #include <Tone.h>
 #include <MOS.h>
 #include "pitches.h" 
@@ -10,7 +11,7 @@ int columns[] = {7,6,5,4,3,2,1,0};
 int rows[] = {A0,A1,A2,A3,A4,A5,10,11}; 
 
 // NOTES FOR BACKGROUND MUSIC
-int bgnotes[] = {
+const PROGMEM uint16_t bgnotes[] = {
   
               NOTE_FS4, NOTE_FS4, 0, NOTE_FS4, NOTE_FS4, NOTE_FS4, 0, NOTE_FS4, 
                 NOTE_E4, NOTE_E4, 0, NOTE_E4, NOTE_E4, NOTE_E4, 0, NOTE_E4, 
@@ -51,7 +52,7 @@ int bgnotes[] = {
                 0,
                 0
                 };
-int bgdur[] = {
+const PROGMEM uint16_t bgdur[] = {
   
            EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, 
               EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, 
@@ -95,10 +96,10 @@ int bgdur[] = {
 int bgidx = 0; 
 int bgstart = millis(); 
 int bgwait = 0;  // gap between notes
-int bgnotes_size = sizeof(bgnotes)/sizeof(int); 
+int bgnotes_size = sizeof(bgnotes)/sizeof(uint16_t); 
 
 // NOTES FOR PLAYER MUSIC
-int mainnotes[] = {
+const PROGMEM uint16_t mainnotes[] = {
   
                 NOTE_FS3, NOTE_FS3, NOTE_FS3, NOTE_FS3, 
                 NOTE_E3, NOTE_E3, NOTE_E3, NOTE_E3, 
@@ -139,7 +140,7 @@ int mainnotes[] = {
                 NOTE_FS4, NOTE_FS4, NOTE_FS4, 0,0,0
                 
                 }; 
-int maindur[] = {
+const PROGMEM uint16_t maindur[] = {
   
                 QUARTER, QUARTER, QUARTER, QUARTER, 
                 QUARTER, QUARTER, QUARTER, QUARTER,
@@ -181,16 +182,39 @@ int maindur[] = {
 int mainidx = 0; 
 int mainstart = millis(); 
 int mainwait = 0; // gap between notes
-int mainnotes_size = sizeof(mainnotes)/sizeof(int); 
+int mainnotes_size = sizeof(mainnotes)/sizeof(uint16_t); 
 
 // gap between notes
 int gapdur = 500-QUARTER; 
 
 
+int letter_O[] = {B01111110, 
+                  B11100111, 
+                  B11000011, 
+                  B11000011, 
+                  B11000011, 
+                  B11000011, 
+                  B11100111,
+                  B01111110};
 
-int speakerPin = A6; 
+int letter_K[] = {B01100011, 
+                  B01100110, 
+                  B01101100, 
+                  B01111000, 
+                  B01101100, 
+                  B01100110, 
+                  B01100011, 
+                  B01100001};
+
+int letter_size = sizeof(letter_O)/sizeof(int); 
 
 Tone notePlayer[2];
+
+int row; 
+int col; 
+int OKidx;
+int prevtime; 
+int currtime; 
 
 void setup() {
   // put your setup code here, to run once:
@@ -225,21 +249,80 @@ void testloop() {
   }
 }
 
+void TestLoop(PTCB tcb) {
+  MOS_Continue(tcb);   
+
+  while (1) { 
+    for(row = 0; row < 8; row++) {
+      for(col = 0; col < 8; col++) {
+        digitalWrite(columns[col], HIGH); 
+        digitalWrite(rows[row], LOW); 
+  
+        MOS_Delay(tcb, 100);
+  
+        digitalWrite(columns[col], LOW); 
+        digitalWrite(rows[row], HIGH); 
+      }
+    }
+  }
+}
+
+void write_OK(PTCB tcb) {
+  MOS_Continue(tcb);  
+  prevtime = millis(); 
+  
+  while (1) {
+    currtime = millis(); 
+    if(currtime < prevtime +1000) {
+      for(OKidx = 0; OKidx < letter_size; OKidx++) {
+        PORTD = letter_O[OKidx];
+        digitalWrite(rows[OKidx], LOW); 
+        
+        MOS_Delay(tcb,1); 
+        
+        PORTD = 0;
+        digitalWrite(rows[OKidx], HIGH); 
+      }
+    }
+
+    else if(currtime >= prevtime+2000) {
+      prevtime = currtime;
+    }
+
+    else if(currtime >= prevtime +1000) {
+    
+      for(OKidx = 0; OKidx < letter_size; OKidx++) {
+        PORTD = letter_K[OKidx];
+        digitalWrite(rows[OKidx], LOW); 
+        
+        MOS_Delay(tcb,1); 
+        
+        PORTD = 0;
+        digitalWrite(rows[OKidx], HIGH); 
+      }
+    }
+
+    
+  }
+
+} 
+
 void PlayMelody1(PTCB tcb) {
   MOS_Continue(tcb);    
   bgidx= 0; 
   while (1) { 
     
     if(bgidx < bgnotes_size) {
-      int currnote = bgnotes[bgidx]; 
+      //int currnote = bgnotes[bgidx]; 
+      uint16_t currnote = pgm_read_word_near(bgnotes + bgidx); 
 
       if(currnote > 0) {
         notePlayer[1].play(currnote);
       } else if (currnote == 0) {
         notePlayer[1].stop();
       }
-      
-      MOS_Delay(tcb, bgdur[bgidx]);
+      uint16_t dur = pgm_read_word_near(bgdur+ bgidx); 
+      MOS_Delay(tcb, dur);
       notePlayer[1].stop();
       MOS_Delay(tcb,gapdur); 
       bgidx++;
@@ -253,15 +336,17 @@ void PlayMelody2(PTCB tcb) {
   bool done = false;
   while (1) {
     if(mainidx < mainnotes_size) {
-      int currnote = mainnotes[mainidx]; 
+      //int currnote = mainnotes[mainidx]; 
+      uint16_t currnote = pgm_read_word_near(mainnotes + mainidx); 
 
       if(currnote > 0) {
         notePlayer[0].play(currnote);
       } else if (currnote == 0) {
         notePlayer[0].stop();
       }
-      
-      MOS_Delay(tcb, maindur[mainidx]);
+
+      uint16_t dur = pgm_read_word_near(maindur+ mainidx); 
+      MOS_Delay(tcb, dur);
       notePlayer[0].stop();
       MOS_Delay(tcb,gapdur); 
       
@@ -276,4 +361,5 @@ void loop() {
   //testloop();  
   MOS_Call(PlayMelody1);
   MOS_Call(PlayMelody2);
+  MOS_Call(write_OK);
 }
